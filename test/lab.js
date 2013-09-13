025232cb49dc8d58517ca73f9d18386ff441576d
Casey Leask <caseyleask@gmail.com>
2013-09-13 22:30:59 +1000

    Added basic test for chasing brains

--- test/lab.js
+++ test/lab.js
@@ -1,1 +1,11 @@
-console.log("No zombies here yet :-(");
+var Express = require('express');
+var express = Express();
+var brains = express.get('/', function(req, res){
+    res.send('hello world');
+    brains.close();
+}).listen(3000);
+var Zombie = require("zombie");
+
+Zombie.visit("http://localhost:3000/", function (e, zombie) {
+  console.log(zombie.html());
+});
